eeee0792fba647dd553bf7d55122118041a1e391
dev-mahbub <mahbub.devs@gmail.com>
2025-08-21 14:02:24 +0600

    first commit

---
+++ src/app/courseStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+const courseStore = (set) => ({
+  courses: [],
+  addCourses: (course) => {
+    set((state) => ({
+      courses: [course, ...state.courses],
+    }));
+  },
+  removeCourses: (coursesId) => {
+    set((state) => ({
+      courses: state.courses.filter((c) => c.id !== coursesId),
+    }));
+  },
+  toggleCourses: (coursesId) => {
+    set((state) => ({
+      courses: state.courses.map((course) =>
+        course.id === coursesId
+          ? { ...course, courseId: !course.courseId }
+          : course
+      ),
+    }));
+  },
+});
+
+const useCourseStore = create(
+  devtools(
+    persist(courseStore, {
+      name: "courses",
+    })
+  )
+);
+
+export default useCourseStore;
